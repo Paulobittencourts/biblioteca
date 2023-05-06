3c018b9527a64fbe29aefbe077bb275ae366df18
Diogo Menezes <diogomenezessilva@hotmail.com>
2023-05-05 22:31:43 -0300

    criação do POST

--- src/main/java/com/br/acervo/biblioteca/controller/UsuarioController.java
+++ src/main/java/com/br/acervo/biblioteca/controller/UsuarioController.java
@@ -2,10 +2,17 @@ package com.br.acervo.biblioteca.controller;
 
 import com.br.acervo.biblioteca.dto.UsuarioDto;
 import com.br.acervo.biblioteca.service.UsuarioService;
+
+import jakarta.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,4 +29,13 @@ public class UsuarioController {
         return userService.getUsuarios();
     }
 
+    @PostMapping
+    public ResponseEntity<UsuarioDto> criarUsuario(@RequestBody @Valid UsuarioDto usuarioDto){
+        UsuarioDto create = userService.criacaoUsuario(usuarioDto);
+        return ResponseEntity.ok(create);
+    }
+
+
+
 }
+
