555533fa8b33b09a24aaa4f063995241f402d59b
paulohbs <paulo.sousa@keyrus.com.br>
2023-05-15 14:48:04 -0300

    Ajustes reserva de livros

--- src/main/java/com/br/acervo/biblioteca/dto/ReservaDto.java
+++ src/main/java/com/br/acervo/biblioteca/dto/ReservaDto.java
@@ -1,9 +1,5 @@
 package com.br.acervo.biblioteca.dto;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,10 +10,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ReservaDto {
-    
-    private long id;
 
-    private long livro;
-
-    private UsuarioDto usuario;
+    private Long id;
+    private LivroDto livro;
 }
